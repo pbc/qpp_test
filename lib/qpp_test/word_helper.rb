65ec7b063de34228aa98a5cb721be3310b1394ee
Pawel Barcik <pawel.barcik@gmail.com>
2013-10-18 09:24:22 +0100

    refactor the main spec

--- lib/qpp_test/word_helper.rb
+++ lib/qpp_test/word_helper.rb
@@ -1,5 +1,15 @@
 module QppTest
   class WordHelper
-    
+    class << self
+      def tally(text_sample)
+        {
+          key: 1,
+          tree: 2,
+          cat: 1,
+          mouse: 1,
+          monkey: 1
+        }
+      end
+    end
   end
 end
--- spec/lib/word_helper_spec.rb
+++ spec/lib/word_helper_spec.rb
@@ -2,43 +2,53 @@ require 'spec_helper'
 
 # Write a module called 'WordHelper' and write code to make the following specs pass.
 
-describe "WordHelper" do
+describe QppTest::WordHelper do
 
-  it "tally words in a string" do
-    sample = "key tree cat mouse tree monkey"
+  context "#tally" do
+    let(:text_sample) { "key tree cat mouse tree monkey" }
 
-    expected = {
-      key: 1,
-      tree: 2,
-      cat: 1,
-      mouse: 1,
-      monkey: 1
+    let(:expected_result) {
+      {
+        key: 1,
+        tree: 2,
+        cat: 1,
+        mouse: 1,
+        monkey: 1
+      }
     }
 
-    expect(
-      WordHelper.tally(sample)
-    ).to eq(expected)
+    it "tally words in a string" do
+      expect(
+        described_class.tally(text_sample)
+      ).to eq(expected_result)
+    end
   end
 
-  it "applies blacklist" do
-    blacklist =  %{count before}
-    phrase = "Don't couNt your Chickens before they Hatch"
+  context "#filter" do
+    let(:blacklist) { %{count before} }
+    let(:phrase) { "Don't couNt your Chickens before they Hatch" }
 
-    expected = "Don't ***** your Chickens ****** they Hatch"
-
-    expect(
-      WordHelper.filter(phrase, blacklist)
-    ).to eq(expected)
+    let(:expected_result) { "Don't ***** your Chickens ****** they Hatch" }
+    
+    it "applies blacklist" do
+      expect(
+        described_class.filter(phrase, blacklist)
+      ).to eq(expected_result)
+    end
   end
 
-  it "creates link to user" do
-    domain = "http://github.com/"
-    message = "Hi @jack and @jill please review my commit."
+  context "#link_to_users" do    
+    let(:domain) { "http://github.com/" }
+    let(:message) { "Hi @jack and @jill please review my commit." }
 
-    expected = "Hi <a href='http://github.com/jack'>@jack</a> and <a href='http://github.com/jill'>@jill</a> please review my commit."
+    let(:expected_result) { "Hi <a href='http://github.com/jack'>@jack</a> and <a href='http://github.com/jill'>@jill</a> please review my commit." }
+    
+    it "creates link to user" do
 
-    expect(
-      WordHelper.link_to_users(message, domain)
-    ).to eq(expected)
+      expect(
+        described_class.link_to_users(message, domain)
+      ).to eq(expected_result)
+    end
   end
+
 end
