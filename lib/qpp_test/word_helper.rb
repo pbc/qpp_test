34fd59045ca7fe21f15a0ae73d92750db5157b2d
Pawel Barcik <pawel.barcik@gmail.com>
2013-10-18 10:09:43 +0100

    make WordHelper#filter work

--- lib/qpp_test/word_helper.rb
+++ lib/qpp_test/word_helper.rb
@@ -1,13 +1,36 @@
 module QppTest
   class WordHelper
     class << self
-      def tally(text_sample)
+      def tally(text)
         tally_result = {}
-        text_sample.split(" ").map(&:strip).each do |word| 
+        extract_words(text).each do |word| 
           key = word.downcase.to_sym
           tally_result[key] = tally_result[key].to_i + 1
         end
         tally_result
+      end
+
+      def filter(phrase, blacklist)
+        filtered_phrase = phrase.dup
+
+        extract_blacklisted_words(phrase, blacklist).each do |word|
+          mask_text = "*" * word.length
+          filtered_phrase.gsub!(word,mask_text)
+        end
+
+        filtered_phrase
+      end
+
+      private
+
+      def extract_blacklisted_words(phrase, blacklist)
+        extract_words(phrase).map do |word|
+          word if blacklist.include? word.downcase
+        end.compact
+      end
+
+      def extract_words(text)
+        text.split(" ").map(&:strip)
       end
     end
   end
